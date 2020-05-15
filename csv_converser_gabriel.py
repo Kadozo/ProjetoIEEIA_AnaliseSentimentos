957ce0fdb5af450a7c0abfc573ed28f6b014a4ca
JoSGomes <58093920+JoSGomes@users.noreply.github.com>
2020-05-15 19:06:23 -0300

    Update csv_converser_gabriel.py

--- csv_converser_gabriel.py
+++ csv_converser_gabriel.py
@@ -9,13 +9,12 @@ cw = csv.writer(open("comentarios.csv", "w"))
 
 # escreve uma linha do csv para cada coluna
 cw.writerow(["Avaliação"] + ["Comentário"])
-
 for a in comentarios:
     linha = a.split(maxsplit=2)
     #remove o : da lista, pois não tem nenhuma utilidade para o csv.
-    linha[1] = linha[2]
+    linha[1] = linha.pop(2)
     #separa toda a string da avaliação em uma lista, em que cada index é uma avaliação
     listAvalliation = linha[0].split(maxsplit=0)
     listAllComments = linha[1].split(maxsplit=0)
     #escreve em cada coluna do arquivo a sua avaliação e seu respectivo comentário.
-    cw.writerow([listAvalliation] + [listAllComments])
+    cw.writerow(listAvalliation + listAllComments)
