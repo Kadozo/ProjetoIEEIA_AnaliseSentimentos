2141582d3367f4079ae4a2b495e228ed670473cf
Marianasls <marianalima0803@gmail.com>
2020-05-16 23:33:53 -0300

    Update csv_converser_gabriel - organizando arquivo csv

--- csv_converser_gabriel.py
+++ csv_converser_gabriel.py
@@ -1,11 +1,11 @@
 import csv
 
 # abre o arquivo correspondente
-file = open("comentarios_bruto.txt", 'r')
+file = open('comentarios_bruto.txt', 'r')
 comentarios = file.readlines()
 
 # csv writer criado
-cw = csv.writer(open("comentarios.csv", "w"))
+cw = csv.writer(open('comentarios.csv', 'w', newline=''), delimiter=' ')
 
 # escreve uma linha do csv para cada coluna
 cw.writerow(["Avaliação"] + ["Comentário"])
@@ -15,6 +15,6 @@ for a in comentarios:
     linha[1] = linha.pop(2)
     #separa toda a string da avaliação em uma lista, em que cada index é uma avaliação
     listAvalliation = linha[0].split(maxsplit=0)
-    listAllComments = linha[1].split(maxsplit=0)
+    listAllComments = linha[1].split()
     #escreve em cada coluna do arquivo a sua avaliação e seu respectivo comentário.
     cw.writerow(listAvalliation + listAllComments)
