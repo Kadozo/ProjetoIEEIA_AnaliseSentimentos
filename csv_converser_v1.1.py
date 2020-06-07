d1f8bea3832109792f2421330776ba0fd15260c1
JoSGomes <58093920+JoSGomes@users.noreply.github.com>
2020-06-07 12:47:08 -0300

    corrigido separação das colunas.
    separação das colunas agora é com vírgulas do que com " ".
    argumento writerow estava recebendo só uma coluna

--- csv_converser_v1.1.py
+++ csv_converser_v1.1.py
@@ -5,12 +5,11 @@ file = open("comentarios_bruto.txt", "r")
 
 
 # csv writer criado
-cw = csv.writer(open('comentarios.csv', 'w', newline=''), delimiter=' ')
+cw = csv.writer(open('comentarios.csv', 'w', newline=''))
 
 # escreve uma linha do csv para cada coluna
-cw.writerow(["Avaliação"] + ["Comentário"])
+cw.writerow(['Avaliação','Comentário'])
 linha = linha = file.readline()
-linhaSep = [" "]
 while(linha != ""):  
     linhaSep = linha.split(maxsplit=2)
       
@@ -20,12 +19,14 @@ while(linha != ""):
     #separa toda a string da avaliação em uma lista, em que cada index é uma avaliação
     Avalliation = linhaSep[0].split(maxsplit=0)
     Comment = linhaSep[1].split(maxsplit=0)
+
     Comment = Comment[0].replace("[", "")
     Comment = Comment.replace("]", "")
     Comment = Comment.replace(".", "")
+    
     CommentToken = nk.word_tokenize(Comment)
+
     #escreve em cada coluna do arquivo a sua avaliação e seu respectivo comentário.
-    cw.writerow(Avalliation + CommentToken)
-    print(Avalliation + CommentToken)
+    cw.writerow([Avalliation, CommentToken])
     linha = file.readline()
 print("Os comentários foram tokenizados e importados para um arquivo .csv!")
