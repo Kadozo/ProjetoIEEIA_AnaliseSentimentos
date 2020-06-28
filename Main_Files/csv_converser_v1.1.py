06e08e1d0cccf18584576b0cdad298752782b043
daniel <38928409+denielfer@users.noreply.github.com>
2020-06-28 06:02:50 -0300

    csv_converser_v1-2.py
    eu mudei a escrita do csv. antes tava usando a escrita do 'csv' e agora eu crio um pandas.DataFrame pra guarda os comentários e avaliações, dai eu boto o pandas pra escrever o escrever esse DF (DataFrame) em csv que ai é só usar a função read_csv do pandas e já carrega o DF

--- Main_Files/csv_converser_v1.1.py
+++ Main_Files/csv_converser_v1.1.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 import nltk as nk
 import re
 
@@ -28,13 +28,12 @@ def remove_caracter(tokens):
 
 
 # abre o arquivo correspondente
-file = open("comentarios_bruto.txt", "r")
+file = open("sent\comentarios_bruto.txt", "r", encoding="UTF-8")
 
-# csv writer criado
-cw = csv.writer(open('comentarios.csv', 'w', newline=''))
+# cria um pandas.DataFrame para armazena os resultados
+df = pd.DataFrame(columns=["Avaliação", "Comentário"])
 
-# escreve uma linha do csv para cada coluna
-cw.writerow(['Avaliação', 'Comentário'])
+
 linha = file.readline()
 while linha != "":
     linhaSep = linha.split(maxsplit=2)
@@ -55,6 +54,67 @@ while linha != "":
     Comment = remove_stopwords(Comment)
     CommentToken = remove_caracter(Comment)
     # escreve em cada coluna do arquivo a sua avaliação e seu respectivo comentário.
-    cw.writerow([Avalliation, CommentToken])
+    df = df.append({"Avaliação":Avalliation,"Comentário":Comment}, ignore_index=True)
     linha = file.readline()
+print(df)
+df.to_csv('sent\comentarios.csv', index=False)
+print("Os comentários foram tokenizados e importados para um arquivo .csv!")import pandas as pd
+import nltk as nk
+import re
+
+
+# Recebe uma lista de token e retorna uma lista de token sem stopwords
+def remove_stopwords(tokens):
+    # Pega a lista de stopwords da biblioteca nltk
+    stopwords = set(nk.corpus.stopwords.words('portuguese'))
+    frase_tratada = []
+    # Qualquer palavra que não esteja na lista de stopwords é adicioinada em frase_tratada
+    for palavra in tokens:
+        if palavra not in stopwords:
+            frase_tratada.append(palavra)
+    return frase_tratada
+
+
+# Recebe uma lista de tokens e retorna uma lista de tokens sem os caracteres especificados
+def remove_caracter(tokens):
+    token_tratado = []
+    for word in tokens:
+        word = re.sub('[.:;.,!@#$"''"]', '', word)
+        token_tratado.append(word)
+    for w in token_tratado:
+        if w == '':
+            token_tratado.remove(w)
+    return token_tratado
+
+
+# abre o arquivo correspondente
+file = open("sent\comentarios_bruto.txt", "r", encoding="UTF-8")
+
+# cria um pandas.DataFrame para armazena os resultados
+df = pd.DataFrame(columns=["Avaliação", "Comentário"])
+
+
+linha = file.readline()
+while linha != "":
+    linhaSep = linha.split(maxsplit=2)
+
+    # remove o : da lista, pois não tem nenhuma utilidade para o csv.
+    linhaSep[1] = linhaSep.pop(2)
+
+    # separa toda a string da avaliação em uma lista, em que cada index é uma avaliação
+    Avalliation = linhaSep[0].split(maxsplit=0)
+    Comment = linhaSep[1].split(maxsplit=0)
+
+    Comment = Comment[0].replace("[", "")
+    Comment = Comment.replace("]", "")
+    Comment = Comment.replace(".", "")
+
+    Tokenizer = nk.TweetTokenizer(reduce_len=3)
+    Comment = Tokenizer.tokenize(Comment)
+    Comment = remove_stopwords(Comment)
+    CommentToken = remove_caracter(Comment)
+    # escreve em cada coluna do arquivo a sua avaliação e seu respectivo comentário.
+    df = df.append({"Avaliação":Avalliation,"Comentário":Comment}, ignore_index=True)
+    linha = file.readline()
+df.to_csv('sent\comentarios.csv', index=False)
 print("Os comentários foram tokenizados e importados para um arquivo .csv!")
