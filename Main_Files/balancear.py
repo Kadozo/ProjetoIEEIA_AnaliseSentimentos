07043b763ff3b4f25d5d077673308feb3a3494d8
daniel <38928409+denielfer@users.noreply.github.com>
2020-07-10 17:08:51 -0300

    Create balancear.py
    nesse arquivo tem uma função que lê o arquivo 'comentarios.csv' e dai retorna um pandas.DataFrame com uma quantidade de recomendados e não recomendados balanceados e eles estão ornados de forma aleatória.

---
+++ Main_Files/balancear.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import random
+def get_balanced():
+    df = pd.read_csv("comentarios.csv")
+    r=0
+    n=0
+    dfR = pd.DataFrame(columns=["Avaliação","Comentário"])
+    dfN = pd.DataFrame(columns=["Avaliação","Comentário"])
+    for a in range(df.last_valid_index()+1):
+        if( df.loc[a]["Avaliação"] ==  "['R']"):
+            dfR.at[r]=df.loc[a].values
+            r+=1
+        elif( df.loc[a]["Avaliação"] ==  "['N']"):
+            dfN.at[n]=df.loc[a].values
+            n+=1
+        else:
+            print(f'Erro at {a}')
+    nwDF=pd.DataFrame(columns=["Avaliação","Comentário"])
+    c=r
+    if(r>n):
+        c=n
+    for a in range(c):
+        nwDF.at[a] = dfR.loc[a].values
+    for a in range(c):
+        nwDF.at[a+c] = dfN.loc[a].values
+    l=[]
+    for a in range(nwDF.last_valid_index()+1):
+        l.append(a)
+    rDF = pd.DataFrame(columns=["Avaliação","Comentário"])
+    c=0
+    m = len(l)
+    for a in range( m ):
+        n = int(random.random()*10000000%len(l))
+        rDF.at[c]=nwDF.loc[l[n]].values
+        c+=1
+        del(l[n])
+    return rDF
