45ef4bc82b513584e642cb6f32cb4d3f5d638c0d
JoSGomes <58093920+JoSGomes@users.noreply.github.com>
2020-07-31 15:08:54 -0300

    acrescentando mostragem de score e dados de teste

--- Main_Files/ClassfierNB.py
+++ Main_Files/ClassfierNB.py
@@ -1,35 +1,42 @@
 import pandas as pd
 import nltk
+import balancear
+import random
 from nltk import word_tokenize
 from nltk.tokenize import TweetTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-file = pd.read_csv('comentarios.csv')
+#pego todos os dados de forma balanceada 50%-50%
+dados = balancear.get_balanced()
+coment=dados["Comentário"]
+aval = dados["Avaliação"]
 
-comments = file["Comentário"]
-avaliation = file["Avaliação"]
+#dados de treino
+dados_treino = coment[:int(len(coment) *.8) ]
+dados_treino_aval = aval[:int(len(aval) *.8) ]
 
+#dados de teste
+dados_test = coment[int(len(coment) *.8): ]
+dados_test_aval = aval[int(len(aval) *.8): ]
+
+#vetorização
 tweet_tokenizer = TweetTokenizer()
 vectorizer = CountVectorizer(analyzer="word", tokenizer = tweet_tokenizer.tokenize)
-freq_comments = vectorizer.fit_transform(comments)
+freq_comments = vectorizer.fit_transform(dados_treino)
+
+#ML
 modelLearn= MultinomialNB()
-modelLearn.fit(freq_comments, avaliation)
+modelLearn.fit(freq_comments, dados_treino_aval)
 
-testes = ['O sistema de montarias é fabuloso!',
-          'Incrível o realismo desse game.',
-          'Dofus é simplesmente amor, que jogo sensacional!',
-          'Que droga de jogo, quem criou isso merece um castigo',
-          'Da pro gasto',
-          'Não gostei.',
-          'Está no começo do jogo, vamos ver se as atualizações irão melhorá-lo']
-
-freq_comments = vectorizer.transform(testes)
-for t, c in zip (testes, modelLearn.predict(freq_comments)):
+#print dos predicts pra cada comentário
+freq_comments = vectorizer.transform(dados_test)
+for t, c in zip (dados_test, modelLearn.predict(freq_comments)):
     print(t + ", " + c)
 
+#array de probabilidades
+print(modelLearn.classes_)
+print(modelLearn.predict_proba(freq_comments).round(2))
 
-
-#print(modelLearn.predict_proba(freq_comments).round(2))
-
-
+#score
+print(modelLearn.score(freq_comments, dados_test_aval))
