a206dc103d608d460f0edf4890d8ca0aade10cdf
JoSGomes <58093920+JoSGomes@users.noreply.github.com>
2020-08-02 16:40:04 -0300

    media com base balanceada e sem balancear

--- Main_Files/ClassfierNB.py
+++ Main_Files/ClassfierNB.py
@@ -7,36 +7,58 @@ from nltk.tokenize import TweetTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-#pego todos os dados de forma balanceada 50%-50%
-dados = balancear.get_balanced()
-coment=dados["Comentário"]
-aval = dados["Avaliação"]
+totalScore = 0
+i=0
+boole = False
 
-#dados de treino
-dados_treino = coment[:int(len(coment) *.8) ]
-dados_treino_aval = aval[:int(len(aval) *.8) ]
+while(i < 10):
+    while(boole == False):
+        resp = input("Deseja dados balanceados? y/n \n")
+        if(resp == "y"):
+            #pego todos os dados de forma balanceada 50%-50%
+            dados = balancear.get_balanced()
+            boole = True
+        elif(resp == "n"):
+            #pega os dados desbalanceados
+            dados = pd.read_csv("comentarios.csv")
+            boole = True
+        else:
+            boole = False
+              
+    coment = dados["Comentário"]
+    aval = dados["Avaliação"]
 
-#dados de teste
-dados_test = coment[int(len(coment) *.8): ]
-dados_test_aval = aval[int(len(aval) *.8): ]
+    #dados de treino
+    dados_treino = coment[:int(len(coment) *.8) ]
+    dados_treino_aval = aval[:int(len(aval) *.8) ]
 
-#vetorização
-tweet_tokenizer = TweetTokenizer()
-vectorizer = CountVectorizer(analyzer="word", tokenizer = tweet_tokenizer.tokenize)
-freq_comments = vectorizer.fit_transform(dados_treino)
+    #dados de teste
+    dados_test = coment[int(len(coment) *.8): ]
+    dados_test_aval = aval[int(len(aval) *.8): ]
 
-#ML
-modelLearn= MultinomialNB()
-modelLearn.fit(freq_comments, dados_treino_aval)
+    #vetorização
+    tweet_tokenizer = TweetTokenizer()
+    vectorizer = CountVectorizer(analyzer="word", tokenizer = tweet_tokenizer.tokenize)
+    freq_comments = vectorizer.fit_transform(dados_treino)
 
-#print dos predicts pra cada comentário
-freq_comments = vectorizer.transform(dados_test)
-for t, c in zip (dados_test, modelLearn.predict(freq_comments)):
-    print(t + ", " + c)
+    #ML
+    modelLearn= MultinomialNB()
+    modelLearn.fit(freq_comments, dados_treino_aval)
 
-#array de probabilidades
-print(modelLearn.classes_)
-print(modelLearn.predict_proba(freq_comments).round(2))
+    #print dos predicts pra cada comentário
+    freq_comments = vectorizer.transform(dados_test)
+    #for t, c in zip (dados_test, modelLearn.predict(freq_comments)):
+       # print(t + ", " + c)
 
-#score
-print(modelLearn.score(freq_comments, dados_test_aval))
+    #array de probabilidades
+    print(modelLearn.classes_)
+    print(modelLearn.predict_proba(freq_comments).round(2))
+
+    #score
+    print(modelLearn.score(freq_comments, dados_test_aval))
+    totalScore += modelLearn.score(freq_comments, dados_test_aval)
+    i += 1
+    boole = False
+
+average = totalScore/10
+print(average)
