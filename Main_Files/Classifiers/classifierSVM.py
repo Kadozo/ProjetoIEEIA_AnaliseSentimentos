5d970f4c03ea593162abcb25a55fdcbd8253b166
Thiago <cardozothiago07@gmail.com>
2020-10-25 18:42:10 -0300

    Update classifierSVM.py

--- Main_Files/Classifiers/classifierSVM.py
+++ Main_Files/Classifiers/classifierSVM.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
 import pickle
-
+import numpy as np
 df = pd.read_csv("DB\\todos_commentarios.csv")
 i = 0
 y = []
@@ -17,8 +17,8 @@ while i<df.shape[0]:
     x.append(df.loc[i,'Comentário'])
     i = i+1
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
-pipe = Pipeline([('counts', CountVectorizer()),('classifier', SVC(kernel='linear',probability=True,random_state=1))])
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = np.random.randint(0,10000))
+pipe = Pipeline([('counts', CountVectorizer()),('classifier', SVC(kernel='linear',probability=True,random_state = np.random.randint(0,10000)))])
 pipe.fit(x_train,y_train)
 print(pipe.score(x_test,y_test))
 
